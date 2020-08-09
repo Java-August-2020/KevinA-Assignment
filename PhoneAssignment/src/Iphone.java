9ed507fcc9f08b6f15a133bca5105da4913c05a7
adegbola <akolade4real@gmail.com>
2020-08-09 14:49:24 -0500

    PhoneAssignment

---
+++ PhoneAssignment/src/Iphone.java
@@ -0,0 +1,21 @@
+public class Iphone extends Phone implements Ringable{
+    public Iphone(String versionNumber, int batteryPercentage, String carrier, String ringTone) {
+        super(versionNumber, batteryPercentage, carrier, ringTone);
+    }
+
+    @Override
+    public void displayInfo() {
+        System.out.println("iPhone " + this.getVersionNumber() + " " + "from " + this.getCarrier());
+
+    }
+
+    @Override
+    public String ring() {
+        return "iPhone " + "says "+ this.getVersionNumber() + " " + this.getRingTone() + "!";
+    }
+
+    @Override
+    public String unlock() {
+        return "Unlocking via facial recognition";
+    }
+}
